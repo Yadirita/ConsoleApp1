afbd1f394704a2017b450a3c77ed1c067edf6599
Denis <Denis@DESKTOP-GR11N4U>
2024-05-15 13:44:02 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Jerarquia/Vehiculo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Jerarquia
+{
+    public class Vehiculo
+    {
+        private String _identificacion;
+        private int _numeroDeMinutos;
+
+
+        public void setIdentificacion(String identificacion)
+        {
+          if(identificacion.Trim().Length > 1)
+            {
+              this._identificacion = identificacion;
+            }
+          else 
+            {
+                this._identificacion = "vacia";
+            }
+        }
+
+        public void setNumeroDeMinutos(int numeroMinutos) 
+        {
+            if (numeroMinutos > 0)
+            {
+                this._numeroDeMinutos = numeroMinutos;
+            }
+            else
+            {
+                this._numeroDeMinutos = 1;
+            }
+        }
+
+        public double dameImporte()
+        {
+            return this._numeroDeMinutos * 0.08;  
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp1.Jerarquia;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hola mundito");
+
+            Vehiculo MiCamion = new Vehiculo();
+            MiCamion.setIdentificacion("A1");
+            MiCamion.setNumeroDeMinutos(200);
+            Console.WriteLine("El camión me debe: " + MiCamion.dameImporte());
+
+
+            Vehiculo MiFurgoneta = new Vehiculo();
+            MiFurgoneta.setIdentificacion("F1");
+            MiFurgoneta.setNumeroDeMinutos(100);
+            Console.WriteLine("La furgoneta me debe: " + MiFurgoneta.dameImporte());
+
+        }
+    }
+}
